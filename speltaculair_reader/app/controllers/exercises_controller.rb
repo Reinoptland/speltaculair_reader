04f7e52af1c3b39d52179d588202e019c735be76
reinoptland <reinoptland@gmail.com>
2016-09-13 13:26:28 +0200

    Fill in excercises_controller

--- speltaculair_reader/app/controllers/exercises_controller.rb
+++ speltaculair_reader/app/controllers/exercises_controller.rb
@@ -1,2 +1,57 @@
 class ExercisesController < ApplicationController
+  before_action :load_exercise, except: [:index, :create]
+
+  def index
+    @exercises = Exercise.all
+    render json: @exercises
+  end
+
+  def show
+    render_excercise
+  end
+
+  def create
+    @exercise = Excercise.new(exercise_params)
+
+    if @exercise.save
+      render_excercise status: :created
+    else
+      render_errors
+    end
+  end
+
+  def update
+    if @exercise.update(exercise_params)
+      render_excercise status: 226
+    else
+      render_errors
+    end
+  end
+
+  def destroy
+    if @exercise.destroy
+      head :ok
+    else
+      render_exercise_errors "Could not destroy this exercise"
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :description)
+  end
+
+  def render_exercise(status:  200)
+    render json: @exercise, status: status
+  end
+
+  def render_exercise_errors(errors = nil)
+    errors ||= @exercise.errors
+    render json: { errors: errors }, status: :unprocessible_entity
+  end
+
+  def load_exercise
+    @exercise = Exercise.find(params[:id])
+  end
 end
