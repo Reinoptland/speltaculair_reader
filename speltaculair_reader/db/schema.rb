84936cc6a36cf519d2c656b211ae170e4445343a
reinoptland <reinoptland@gmail.com>
2016-09-13 10:59:17 +0200

    Add report model

--- speltaculair_reader/db/schema.rb
+++ speltaculair_reader/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913085046) do
+ActiveRecord::Schema.define(version: 20160913085747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20160913085046) do
     t.text     "solution_text"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.integer  "current_step"
+    t.boolean  "finished"
+    t.integer  "hints_asked"
+    t.integer  "user_id"
+    t.integer  "exercise_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["exercise_id"], name: "index_reports_on_exercise_id", using: :btree
+    t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
   end
 
   create_table "steps", force: :cascade do |t|
@@ -54,4 +66,6 @@ ActiveRecord::Schema.define(version: 20160913085046) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "reports", "exercises"
+  add_foreign_key "reports", "users"
 end
