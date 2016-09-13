6344e6e5e231dcb39b11604513324cce9acfc697
reinoptland <reinoptland@gmail.com>
2016-09-13 12:46:46 +0200

    Add resources to routes

--- speltaculair_reader/config/routes.rb
+++ speltaculair_reader/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users do
+    resources :reports
+  end
+  
+  resources :exercises do
+      resources :steps do
+        resources :hints
+      end
+  end
+
 end
