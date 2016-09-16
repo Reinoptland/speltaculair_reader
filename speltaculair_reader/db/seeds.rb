df4e9a195f5b2e1667b9ba99ee8fd8069cf35ab6
reinoptland <reinoptland@gmail.com>
2016-09-16 02:53:50 +0200

    Update seeddb

--- speltaculair_reader/db/seeds.rb
+++ speltaculair_reader/db/seeds.rb
@@ -9,6 +9,8 @@ rein = User.create( email: 'rein@test.com', password: 'abcd1234' )
 Exercise.create([
   {name: "Loops", description: "Learn how to loop!"},
   {name: "Variables", description: "Learn to pronounce variables properly" },
+  {name: "Something Cool", description: "More exercises here" },
+  {name: "A final test", description: "And another one!" },
 ])
 
 Report.create([
@@ -17,10 +19,14 @@ Report.create([
 
 Step.create([
   {instruction: "Here we go! We're looping!", completed: false, exercise_id: 1, },
+  {instruction: "I should tell you to do something I guess", completed: false, exercise_id: 1, },
+  {instruction: "And some more here..", completed: false, exercise_id: 1, },
   ])
 
 Hint.create([
   {hint_text: "so here is a hint on loops..", solution_text: "and here is the solution", solution: "an image url with th picture resides here", step_id: 1, },
+  {hint_text: "Another hint here!", solution_text: "and here is the solution", solution: "an image url with th picture resides here", step_id: 2, },
+  {hint_text: "And the final one!", solution_text: "and here is the solution", solution: "an image url with th picture resides here", step_id: 3, },
   ])
 
 puts "created #{User.all.length} users"
