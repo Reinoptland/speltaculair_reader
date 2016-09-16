9f95c1efe089ac5daf4f270c37c382ba615be343
reinoptland <reinoptland@gmail.com>
2016-09-16 06:04:02 +0200

    Edit NavBar component

--- speltaculair_reader_react/src/components/Navbar.js
+++ speltaculair_reader_react/src/components/Navbar.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import { connect } from 'react-redux'
 
-const Navbar = () => (
-  <AppBar
-    title="Speltaculair Scratch Reader!!"
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={
-      <IconMenu
-        iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
-      </IconMenu>
-    }
-  />
-);
+class Navbar extends Component {
+  render() {
+      return (
+        <AppBar
+          title="Speltaculair Scratch Reader!!"
+          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="Refresh" />
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+            </IconMenu>
+          }
+        />
+    )
+  }
+}
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, {})(Navbar)
